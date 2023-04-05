966ab1423f34f3e289b5dcf2c043338ca21f1151
KGranger56 <kelly.granger56@gmail.com>
2023-04-05 13:32:36 -0500

    Element Manipulation Example Updated

--- Projects/Element Manipulation/index.js
+++ Projects/Element Manipulation/index.js
@@ -1,15 +1,14 @@
 let list = document.getElementsByTagName('li');
 let pick = document.getElementsByClassName("pickme");
-let button = document.getElementsByTagName('button')
+let button = document.getElementsByTagName('button');
+let list2 = document.getElementsByClassName('second');
+let secondList= list2[0].children;
 
 function Manipulation(){
   for(let v=0; v < list.length; v++){
     list[v].setAttribute('id', '#');
-    console.log(list)
-  }
-
-  for(let v=0; v < list.length; v++){
     list[v].innerHTML=('list item #' + v)
+    console.log(list)
   }
 
   for(let v=0; v < list.length; v++){
@@ -18,3 +17,18 @@ function Manipulation(){
 
   pick[0].remove();
 }
+
+
+function Classes(){
+  for(let v=0; v < secondList.length; v++){
+    if(secondList[v].className =='first'){
+      secondList[v].innerHTML=secondList[v].className
+      secondList[v].setAttribute('class', 'first test1 test4');
+    }
+    else{
+      secondList[v].innerHTML='no Class'
+      secondList[v].setAttribute('class', 'test1 test4');
+    }
+    console.log(secondList);
+  }
+};
