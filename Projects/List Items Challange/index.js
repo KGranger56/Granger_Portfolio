77da525acfe6de0251851e4459dc7c8aa5ec2143
KGranger56 <kelly.granger56@gmail.com>
2023-04-06 10:12:51 -0500

    Background Color Change Button Added

--- Projects/List Items Challange/index.js
+++ Projects/List Items Challange/index.js
@@ -18,49 +18,14 @@ function AddItem(){
   }
 }
 
-/*
-function Manipulation(){
-  for(let v=0; v < list.length; v++){
-    list[v].setAttribute('id', '#');
-    list[v].innerHTML=('list item #' + v)
-    console.log(list)
-  }
+//change background color
+let body = document.getElementsByTagName('body')[0];
+let bgColor = 'rgb(' + random(255) + ',' + random(255) + ','+ random(255) + ')';
 
-  for(let v=0; v < list.length; v++){
-    console.log(list[v].className)
-  }
-
-  pick[0].remove();
+function Update(){
+  body.style.backgroundColor=bgColor;
 }
 
-
-//function for list 2
-function Classes(){
-  for(let v=0; v < secondList.length; v++){
-    if(secondList[v].className =='first'){
-      secondList[v].innerHTML=secondList[v].className
-      secondList[v].setAttribute('class', 'first test1 test4');
-    }
-    else{
-      secondList[v].innerHTML='no Class'
-      secondList[v].setAttribute('class', 'test1 test4');
-    }
-    console.log(secondList);
-  }
-};
-
-//Adding a new element
-let h1 = document.getElementsByTagName('h1')[0]
-let div = document.createElement('div')
-
-function AddElement(){
-
-const textInside=document.createTextNode('Hello World')
-
-div.style.color="blue";
-div.style.fontSize="2rem";
-div.setAttribute('id', 'test')
-div.appendChild(textInside)
-
-h1.before(h1, div)
-}*/
+function random(bgColor){
+  return Math.floor(Math.random() * (bgColor +1));
+}
